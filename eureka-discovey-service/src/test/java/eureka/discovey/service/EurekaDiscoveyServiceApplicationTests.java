3717a2acb216ae56fd78be41aa9c2cbfcca76c21
Sholontla <64672755+Sholontla@users.noreply.github.com>
2022-04-18 14:10:11 -0500

    Add files via upload

---
+++ eureka-discovey-service/src/test/java/eureka/discovey/service/EurekaDiscoveyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eureka.discovey.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
