828b09689534f5f524e9ff1cfc7a46155ee14b6c
Sholontla <64672755+Sholontla@users.noreply.github.com>
2022-04-18 14:11:53 -0500

    Add files via upload

---
+++ service-config-server/src/test/java/service/config/service/ServiceConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package service.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
