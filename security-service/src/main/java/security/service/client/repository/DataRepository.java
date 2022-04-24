29ef7aad01c028781a56dd9dd1e9643584a8ee52
Gerardo Ruiz Bustani <64672755+Sholontla@users.noreply.github.com>
2022-04-24 18:53:56 -0500

    Update DataRepository.java

--- security-service/src/main/java/security/service/client/repository/DataRepository.java
+++ security-service/src/main/java/security/service/client/repository/DataRepository.java
@@ -1,10 +1,10 @@
 package security.service.client.repository;
 
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import security.service.client.data.DataEntity;
-
-public interface DataRepository  extends CrudRepository<DataEntity, Long>{
-	
+@Repository
+public interface DataRepository  extends JpaRepository<DataEntity, Long>{
+	List<DataEntity> findByGroup(Integer group);
 }
